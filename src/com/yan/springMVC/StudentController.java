784108f670d78165fa8db7b946797abc3fdf6fcd
Yanbin Deng <dyb0214@gmail.com>
2018-10-30 08:47:35 -0700

    added @InitBinder form validation

--- src/com/yan/springMVC/StudentController.java
+++ src/com/yan/springMVC/StudentController.java
@@ -2,9 +2,12 @@ package com.yan.springMVC;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +18,14 @@ import com.yan.springMVC.beans.Student;
 @RequestMapping("/student")
 public class StudentController {
 	
+	// ===== Use the @InitBinder to convert trim input strings 
+	// remove leading and trailing whitespaces and resolve issue for our validation 
+	// THIS method will be called for every incoming request for the controller  
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor se = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, se);
+	}
 	
 	//STEP 3-1
 	@RequestMapping("/showForm")
